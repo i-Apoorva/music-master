67828d44a8f4c40920576e8f4902a1606a800ae7
i-Apoorva <apoorva.ambhoj95@gmail.com>
2019-10-08 10:25:58 +0530

    added artist component

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import Artist from './Artist';
+
+const API_ADDRESS='https://spotify-api-wrapper.appspot.com/';
+class App extends Component {
+   
+state= {artistQuery: '', artist:null, tracks:[]};
+
+     updateArtistQuery=(event)=> {
+         this.setState({artistQuery: event.target.value});
+     }
+
+     handleKeyPress=(event) =>{
+         if(event.key== 'Enter') {
+             this.searchArtist();
+         }
+     }
+
+     searchArtist=()=>{
+         fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+         .then(response=>response.json())
+         .then(json=> {
+            if(json.artists.total >0){
+                const artist= json.artists.items[0];
+                this.setState({artist});
+                console.log('artist', artist);
+
+                fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                .then(response=>response.json())
+         .then(json=> this.setState({tracks: json.tracks}))
+         .catch(error=> alert(error.message));
+            }
+        }
+            )
+            .catch(error=> alert(error.message));
+     }
+
+    render() {
+        console.log('state is',this.state);
+        return(
+            <div>
+               <h1>Music Master</h1>
+               <input type="text" 
+                onChange={this.updateArtistQuery} 
+                onKeyPress={this.handleKeyPress}
+                placeholder="Enter the artist" />
+               <button onClick={this.searchArtist}>Search</button>
+               <Artist artist={this.state.artist}/>
+            </div>
+        ); 
+    }
+}
+export default App;
